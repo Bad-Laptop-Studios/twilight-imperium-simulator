867af103cdbd510205db2d0a0b37714bb0fe10a9
Pegadari <71580795+Pegadari@users.noreply.github.com>
2022-06-18 14:10:46 +1000

    Outlined Faction class
    + Faction class
    + action-card-base.json

---
+++ factions/faction.py
@@ -0,0 +1,28 @@
+import using
+
+class Faction():
+    def __init__(self):
+        self._name = None
+        self._abilities = None
+        self._promissory_note = None
+        self._faction_technologies = None
+        self._faction_units = None
+        self._flagship = None
+        if using.POK:
+            self.mech = None
+            self.learders = None
+
+        self._commodities = None
+        self._starting_units = None
+        self.starting_technologies = None
+        self.starting_planets = None
+
+
+        self.quote = None
+        self.lore = None
+
+        pass
+
+    def get_commodities(self) -> int:
+        """ Returns the faction's default commodity value. """
+        return self.commodities
---
+++ using.py
@@ -0,0 +1,9 @@
+# stripped down versions
+
+
+# actual game versions
+BASE      = False
+POK       = False
+CODEX_I   = False
+CODEX_II  = False
+CODEX_III = False
