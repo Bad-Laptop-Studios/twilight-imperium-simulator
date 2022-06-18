500bd7e1bb8f66e680af40a9152bd26d4ba167df
Pegadari <71580795+Pegadari@users.noreply.github.com>
2022-06-18 17:37:35 +1000

    Working on Strategy Cards classes
    Outlined how strategy cards are implemented

--- strategy-cards.py
+++ strategy-cards.py
@@ -1,7 +1,19 @@
+from typing import *
+
+from Manager import Player
+
 class StrategyCard():
     def __init__(self):
-        self._name = None
+        self._name = __name__
         self._is_exhausted = False
+        self._primary_text = None
+        self._secondary_text = None
+
+    def get_primary_text(self) -> List[str]:
+        return self._primary_text
+
+    def get_secondary_text(self) -> List[str]:
+        return self._secondary_text
 
     def resolve_primary(self):
         raise NotImplementedError
@@ -11,13 +23,20 @@ class StrategyCard():
 
 class Leadership(StrategyCard):
     def __init__(self):
-        pass
+        self._primary_text = ["Gain 3 command tokens.", "Spend any amount of influence to gain 1 command token for every 3 influence spent"]
+        self._primary_code = "" # custom programming language to represent above
+        self._secondary_text = ["Spend any amount of influence to gain 1 command token for every 3 influence spent"]
+        self._secondary_code = "" # custom programming language to represent above
 
-    def resolve_primary(self):
-        raise NotImplementedError
+    def resolve_primary(self, player: Player) -> None:
+        player.change_commodities(3) # method does not exist yet
+        self.resolve_secondary(player)
 
-    def resolve_secondary(self):
-        raise NotImplementedError
+    def resolve_secondary(self, player: Player) -> None:
+        # the following is NOT actual Python code
+        # the ability depends on implementation of the calling class
+        value = "however this INTEGER is calculated"
+        player.change_command_tokens(value)
 
 class Diplomacy(StrategyCard):
     def __init__(self):
@@ -31,13 +50,19 @@ class Diplomacy(StrategyCard):
     
 class Politics(StrategyCard):
     def __init__(self):
+        self._primary_text = ["Choose a player other than the speaker. That player gains the speaker token.", "Draw 2 action cards.", "Look at the top 2 cards of the agenda deck. Place each card on the top or bottom of the deck in any order"]
+        self._primary_code = "" # custom programming language to represent above
+        self._secondary_text = ["Spend 1 token from your strategy pool to draw 2 action cards."]
+        self._secondary_code = "" # custom programming language to represent above
+
+    def resolve_primary(self, player: Player) -> None:
+        # ability 1
+        # ability 2
+        player.draw_action_cards(2) # method does not exist yet
+        # ability 3
+
+    def resolve_secondary(self, player: Player) -> None:
         pass
-
-    def resolve_primary(self):
-        raise NotImplementedError
-
-    def resolve_secondary(self):
-        raise NotImplementedError
 
 class Construction(StrategyCard):
     def __init__(self):
